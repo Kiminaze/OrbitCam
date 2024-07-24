f88f4e34acbd0ed76681cc3a1cae305b2a2087c7
Kiminaze <kiminaze@yahoo.de>
2024-07-24 05:20:01 +0200

    - version update to 1.1.0 - added lua54 entry - added use_experimental_fxv2_oal entry

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,10 @@ games { "gta5" }
 
 author "Philipp Decker"
 description "Allows toggling an orbit camera around a given point or entity."
-version "1.0.1"
+version "1.1.0"
+
+lua54 "yes"
+use_experimental_fxv2_oal "yes"
 
 client_scripts {
 	"config.lua",
