4f1f220c0a74d8f8b9e8f75de7b2b6166af7b6f5
Kiminaze <kiminaze@yahoo.de>
2025-04-06 20:58:32 +0200

    Added autoOrbitCam that automatically orbits around the player

---
+++ cameras/autoOrbitCam.lua
@@ -0,0 +1,27 @@
+﻿
+if (not autoOrbitCam) then return end
+
+-- key binding (see https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/)
+local INPUT_MAPPER <const> = "KEYBOARD"
+local INPUT_KEYBIND <const> = "O"
+local INPUT_DESCRIPTION <const> = "Start orbit cam"
+
+-- focus point offset from the entity
+local OFFSET <const> = vector3(0, 0, 0.5)
+
+-- speed at which the camera should move on the orbit
+local ORBIT_SPEED <const> = 1.0
+
+local orbitCam = exports["OrbitCam"]
+RegisterCommand("+startorbit", function(src, args, raw)
+	if (orbitCam:IsOrbitCamActive()) then
+		orbitCam:EndOrbitCam()
+		return
+	end
+	
+	orbitCam:StartOrbitCam(OFFSET, PlayerPedId())
+	orbitCam:SetAutoOrbitSpeed(ORBIT_SPEED)
+end, false)
+RegisterCommand("-startorbit", function() end, false)
+
+RegisterKeyMapping("+startorbit", INPUT_DESCRIPTION, INPUT_MAPPER, INPUT_KEYBIND)
--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-
+﻿
 -- default min and max radius the camera will orbit at (in meters)
 defaultMinRadius = 2.0
 defaultMaxRadius = 7.0
@@ -15,3 +15,6 @@ controllerSpeed	= 1.5
 
 -- auto toggles the camera when the player is dead (only works when using the baseevents resource)
 deathCam = true
+
+-- start an automatic orbit cam via button press (see autoOrbitCam.lua for details)
+autoOrbitCam = false
